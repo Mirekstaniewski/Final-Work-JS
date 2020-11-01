a57cea129f6966c8a0c0c206ca8e6c4cb1de7db9
Mirekstaniewski <mirekstaniewski@gmail.com>
2020-11-02 00:32:06 +0300

    attempt to fix set timer Values

--- js/app.js
+++ js/app.js
@@ -14,6 +14,20 @@ class Timer {
     this.iconsPath = "./icons/sprite.svg#";
 
     this.isEdit = true;
+    this.isCounting = false;
+
+    this.interval = null;
+
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.totalTime = 0;
+    this.currentTime = 0;
+    this.maxSeconds = 60;
+    this.maxMinutes = 60;
+    this.maxHours = 99;
+    this.maxTime =
+      this.maxHours * 3600 + (this.maxMinutes - 1) * 60 + this.maxSeconds - 1;
 
     this.UiSelectors = {
       hours: " hours ",
@@ -66,15 +80,50 @@ class Timer {
     }
     this.selectUseElement(this.editBtn).setAttribute(
       "xlink:href",
-      `${this.iconsPath}create-24px`
+      `${this.iconsPath}done-24px`
     );
     this.timerInputs.forEach((timerInput) => {
       timerInput.setAttribute("disabled", " ");
     });
     this.runBtn.removeAttribute("disabled");
+
+    this.getTimerValues();
+    this.setTimerValues();
   }
 
   selectUseElement(element) {
     return element.querySelector(" use ");
+  }
+
+  // функция которая забирает время с инпутов
+
+  getTimerValues() {
+    this.hours = parseInt(this.hoursInput.value, 10);
+    this.minutes = parseInt(this.minutesInput.value, 10);
+    this.seconds = parseInt(this.secondsInput.value, 10);
+
+    this.countTotalTime();
+  }
+  /* при вводе простого числа добовляем в начале 0 */
+
+  setTimerValues() {
+    const seconds = `0${this.currentTime % this.maxSeconds}`;
+    const minutes = `0${Math.floor(this.currentTime / 60) % this.maxMinutes}`;
+    const hours = `0${Math.floor(this.currentTime / 3600) % this.maxHours}`;
+
+    /* урезаем 2 знака */
+
+    this.secondsInput.value = seconds.slice(-2);
+    this.minutesInput.value = minutes.slice(-2);
+    this.hoursInput.value = hours.slice(-2);
+  }
+
+  countTotalTime() {
+    const timeSum = this.seconds + this.minutes * 60 + this.hours * 3600;
+    // если установить больше чем максимальное возвращает максимальное число
+    this.totalTime = timeSum <= this.maxTime ? timeSum : this.maxTime;
+
+    // изначальное текущее время равно установленному времени
+    this.currentTime = this.totalTime;
   }
 }
